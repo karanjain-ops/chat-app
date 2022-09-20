3adfe62887670f5d339eb7468e3d294f43a4cb7b
karanjain-ops <jkaran.2349@gmail.com>
2022-09-21 01:38:18 +0530

    Adding search.php

--- users.php
+++ users.php
@@ -1,15 +1,26 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['unique_id'])){
+        header("location: login.php");
+    }
+?>
 <?php include_once "header.php";?>
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
             <?php
+                include_once "php/config.php";
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                if(mysqli_num_rows($sql)>0){
+                    $row = mysqli_fetch_assoc($sql);
+                }
             ?>
                 <div class="content">
-                    <img src="C:\Users\karan\Downloads\passport size.png" alt="">
+                    <img src="php/images/<?php echo $row['img'];?>" alt="">
                     <div class="details">
-                        <span>Karan Jain</span>
-                        <p>Active Now</p>
+                        <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
+                        <p><?php echo $row['status'];?></p>
                     </div>
                 </div>
                 <a href="#" class="logout">Logout</a>
@@ -20,76 +31,6 @@
                 <button><i class="fas fa-search"></i></button>
             </div>
             <div class="users-list">
-                <a href="#">
-                    <div class="content">
-                        <img src="C:\Users\karan\Downloads\passport size.png" alt="">
-                        <div class="details">
-                            <span>Karan Jain</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="C:\Users\karan\Downloads\passport size.png" alt="">
-                        <div class="details">
-                            <span>Karan Jain</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="C:\Users\karan\Downloads\passport size.png" alt="">
-                        <div class="details">
-                            <span>Karan Jain</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="C:\Users\karan\Downloads\passport size.png" alt="">
-                        <div class="details">
-                            <span>Karan Jain</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="C:\Users\karan\Downloads\passport size.png" alt="">
-                        <div class="details">
-                            <span>Karan Jain</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="C:\Users\karan\Downloads\passport size.png" alt="">
-                        <div class="details">
-                            <span>Karan Jain</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="C:\Users\karan\Downloads\passport size.png" alt="">
-                        <div class="details">
-                            <span>Karan Jain</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
             </div>
         </section> 
     </div>

0c658c93052622c86bc40c4a655b6a8d088d50fb
karanjain-ops <jkaran.2349@gmail.com>
2022-09-21 00:38:00 +0530

    Adding php code

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content=""width=device-width, initial-scale="1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Chat App</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-</head>
+<?php include_once "header.php";?>
 <body>
     <div class="wrapper">
         <section class="form signup">
             <header>Chat App</header>
-            <form action="#">
-                <div class="error-txt">This is an error message!</div>
+            <form action="#" enctype="multipart/form-data" autocomplete="off">
+                <div class="error-txt"></div>
                 <div class="name-details">
                     <div class="field input">
                         <label for="fname">First Name</label>
@@ -41,9 +32,10 @@
                     <input type="submit" name="submit" value="Continue to Chat">
                 </div>
             </form>
-            <div class="link">Already signed up? <a href="#">Login now</a></div>
+            <div class="link">Already signed up? <a href="login.php">Login now</a></div>
         </section>
     </div>
     <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/signup.js"></script>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content=""width=device-width, initial-scale="1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Chat App</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-</head>
+<?php include_once "header.php";?>
 <body>
     <div class="wrapper">
         <section class="form login">
@@ -27,10 +18,12 @@
                     <input type="submit" name="submit" value="Continue to Chat">
                 </div>
             </form>
-            <div class="link">Not yet signed up? <a href="#">Sign Up</a></div>
+            <div class="link">Not yet signed up? <a href="index.php">Sign Up</a></div>
         </section>
     </div>
 
     <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/login.js"></script>
+
 </body>
 </html>
